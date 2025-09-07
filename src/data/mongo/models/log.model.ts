397733a8cd162cd6a34fca2b0720803939caa228
Andres Felipe Estrada <andresfe1396@gmail.com>
2025-09-06 19:11:58 -0500

    fix: add LogEntity in mongoose.Schemea

--- src/data/mongo/models/log.model.ts
+++ src/data/mongo/models/log.model.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import type { LogEntity } from "../../../domain/entities/log.entity";
 
-const logScheme = new mongoose.Schema({
+const logScheme = new mongoose.Schema<LogEntity>({
 	message: { type: String, required: true },
 	level: { type: String, enum: ["LOW", "MEDIUM", "HIGH"], default: "LOW" },
 	origin: { type: String },
