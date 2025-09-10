e1003bea7d274b7b38a2aca03fd16d0845ddaebb
Andres Felipe Estrada <andresfe1396@gmail.com>
2025-09-09 19:56:36 -0500

    fix: delete trycatch

--- src/data/mongo/init.ts
+++ src/data/mongo/init.ts
@@ -1,25 +1,21 @@
 import mongoose from "mongoose";
 
 interface ConnectionOptions {
-	mongoUrl: string;
-	dbName: string;
+  mongoUrl: string;
+  dbName: string;
 }
 
 export class MongoDatabase {
-	static async connect(options: ConnectionOptions) {
-		try {
-			await mongoose.connect(options.mongoUrl, {
-				dbName: options.dbName,
-			});
-			console.log("Mongo connected!");
-			return true;
-		} catch (error) {
-			throw error;
-		}
-	}
+  static async connect(options: ConnectionOptions) {
+    await mongoose.connect(options.mongoUrl, {
+      dbName: options.dbName,
+    });
+    console.log("Mongo connected!");
+    return true;
+  }
 
-	static async disconnect() {
-		await mongoose.disconnect();
-    return true
-	}
+  static async disconnect() {
+    await mongoose.disconnect();
+    return true;
+  }
 }
